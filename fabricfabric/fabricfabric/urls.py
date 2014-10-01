8acbb823962a8809ac0b85865912e8b61dbe6d43
wikibootup <wikibootup@gmail.com>
2014-10-01 13:30:33 +0900

    some error fixed

--- fabricfabric/fabricfabric/urls.py
+++ fabricfabric/fabricfabric/urls.py
@@ -5,12 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 from fabricfabric.views import Home
-from server.views import Logout
 import server.views
 
 urlpatterns = patterns('',
 #    url(r'^$', server.views.index, name='index'),
     url(r'^$', Home.as_view(), name='home'),
-    url(r'^logout/', Logout.as_view(), name="logout"),    
 )
 
